0912dca2f8511952912ce113d9c7ee63fc674903
Fran <fran.bosancic@tvz.hr>
2025-06-21 16:24:44 +0200

    popravljen sql file tako da baza cijela moze importat

--- baza/pwa_projekt.sql
+++ baza/pwa_projekt.sql
@@ -11,7 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 CREATE DATABASE IF NOT EXISTS `pwa_projekt` DEFAULT CHARACTER SET utf8 COLLATE utf8_croatian_ci;
 USE `pwa_projekt`;
 
@@ -20,24 +19,20 @@ USE `pwa_projekt`;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `pwa_projekt`
---
+DROP TABLE IF EXISTS `kategorija`;
+DROP TABLE IF EXISTS `story`;
+DROP TABLE IF EXISTS `users`;
 
 -- --------------------------------------------------------
-
---
 -- Table structure for table `kategorija`
---
+-- --------------------------------------------------------
 
 CREATE TABLE `kategorija` (
   `id` int(11) NOT NULL,
   `naziv` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
 
---
 -- Dumping data for table `kategorija`
---
 
 INSERT INTO `kategorija` (`id`, `naziv`) VALUES
 (1, 'News'),
@@ -45,10 +40,8 @@ INSERT INTO `kategorija` (`id`, `naziv`) VALUES
 (3, 'Sport');
 
 -- --------------------------------------------------------
-
---
 -- Table structure for table `story`
---
+-- --------------------------------------------------------
 
 CREATE TABLE `story` (
   `ID` int(11) NOT NULL,
@@ -60,18 +53,14 @@ CREATE TABLE `story` (
   `arhiva` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
 
---
 -- Dumping data for table `story`
---
 
 INSERT INTO `story` (`ID`, `naslov`, `sazetak`, `tekst`, `kategorija_id`, `slika`, `arhiva`) VALUES
 (19, 'dfgdfg', 'dfgdfg', 'fdgdfg', 1, '', 0);
 
 -- --------------------------------------------------------
-
---
 -- Table structure for table `users`
---
+-- --------------------------------------------------------
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -79,9 +68,7 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
 
---
 -- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
 (4, 'admin', '$2y$10$Pgzs20fMVeXwzYSYg1NKReNasuA/JNg8SHm5p3G54lijf25l7LXvq'),
@@ -89,61 +76,41 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 (7, 'proba123', '$2y$10$v6jXI27sg2EPXWUVOgvTCeaQdQMjx3sBfEIyTJQ0nSrqx6VPALsgq'),
 (8, 'a', '$2y$10$kdv7JSPnp6cIc/2q26z.GufyxMXSckiqze4LyiDNW2KQldMcYdTz6');
 
---
+-- --------------------------------------------------------
 -- Indexes for dumped tables
---
+-- --------------------------------------------------------
 
---
--- Indexes for table `kategorija`
---
 ALTER TABLE `kategorija`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `story`
---
 ALTER TABLE `story`
   ADD PRIMARY KEY (`ID`),
   ADD KEY `fk_kategorija` (`kategorija_id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
---
+-- --------------------------------------------------------
 -- AUTO_INCREMENT for dumped tables
---
+-- --------------------------------------------------------
 
---
--- AUTO_INCREMENT for table `kategorija`
---
 ALTER TABLE `kategorija`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT for table `story`
---
 ALTER TABLE `story`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
---
+-- --------------------------------------------------------
 -- Constraints for dumped tables
---
+-- --------------------------------------------------------
 
---
--- Constraints for table `story`
---
 ALTER TABLE `story`
   ADD CONSTRAINT `fk_kategorija` FOREIGN KEY (`kategorija_id`) REFERENCES `kategorija` (`id`) ON UPDATE CASCADE;
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
